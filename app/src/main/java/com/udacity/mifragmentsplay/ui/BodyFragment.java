e294e99f19f962d5136e268a719faf9963a1a284
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-04-13 17:06:25 +0530

    Configuration Changes handled, Selected images remain intact as device rotated

--- app/src/main/java/com/udacity/mifragmentsplay/ui/BodyFragment.java
+++ app/src/main/java/com/udacity/mifragmentsplay/ui/BodyFragment.java
@@ -12,12 +12,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.udacity.mifragmentsplay.R;
-import com.udacity.mifragmentsplay.data.AndroidImageAssets;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BodyFragment extends Fragment {
 
+    public static final String IMAGE_ID_LIST = "image_ids";
+    public static final String LIST_INDEX = "list_index";
     private static final String TAG = "BodyFragment";
     private List<Integer> mImageIds;
     private int mListIndex;
@@ -28,25 +30,28 @@ public class BodyFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            mImageIds = savedInstanceState.getIntegerArrayList(IMAGE_ID_LIST);
+            mListIndex = savedInstanceState.getInt(LIST_INDEX, 0);
+        }
+
         View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
         final ImageView imageView = rootView.findViewById(R.id.body_part_image_view);
 
         imageView.setImageResource(mImageIds.get(mListIndex));
-        if(mImageIds!=null) {
+        if (mImageIds != null) {
             imageView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    if(mListIndex<mImageIds.size()-1){
+                    if (mListIndex < mImageIds.size() - 1) {
                         mListIndex++;
-                    }
-                    else{
-                        mListIndex=0;
+                    } else {
+                        mListIndex = 0;
                     }
                     imageView.setImageResource(mImageIds.get(mListIndex));
                 }
             });
-        }
-        else{
+        } else {
             Log.d(TAG, "The fragment has null list of imageIds.");
         }
         return rootView;
@@ -58,5 +63,11 @@ public class BodyFragment extends Fragment {
 
     public void setmListIndex(int mListIndex) {
         this.mListIndex = mListIndex;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putIntegerArrayList(IMAGE_ID_LIST, (ArrayList<Integer>) mImageIds);
+        outState.putInt(LIST_INDEX, mListIndex);
     }
 }
--- app/src/main/java/com/udacity/mifragmentsplay/ui/MainActivity.java
+++ app/src/main/java/com/udacity/mifragmentsplay/ui/MainActivity.java
@@ -1,7 +1,9 @@
 package com.udacity.mifragmentsplay.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.udacity.mifragmentsplay.R;
 import com.udacity.mifragmentsplay.data.AndroidImageAssets;
 
@@ -12,28 +14,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BodyFragment headFragment = new BodyFragment();
-        headFragment.setmImageIds(AndroidImageAssets.getHeads());
-        headFragment.setmListIndex(0);
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.header_container,headFragment)
-                .commit();
+        if (savedInstanceState == null) {
+            BodyFragment headFragment = new BodyFragment();
+            headFragment.setmImageIds(AndroidImageAssets.getHeads());
+            headFragment.setmListIndex(0);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.header_container, headFragment)
+                    .commit();
 
-        BodyFragment bodyFragment = new BodyFragment();
-        bodyFragment.setmImageIds(AndroidImageAssets.getBodies());
-        bodyFragment.setmListIndex(0);
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.body_container,bodyFragment)
-                .commit();
+            BodyFragment bodyFragment = new BodyFragment();
+            bodyFragment.setmImageIds(AndroidImageAssets.getBodies());
+            bodyFragment.setmListIndex(0);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.body_container, bodyFragment)
+                    .commit();
 
-        BodyFragment legsFragment = new BodyFragment();
-        legsFragment.setmImageIds(AndroidImageAssets.getLegs());
-        legsFragment.setmListIndex(0);
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.legs_container,legsFragment)
-                .commit();
+            BodyFragment legsFragment = new BodyFragment();
+            legsFragment.setmImageIds(AndroidImageAssets.getLegs());
+            legsFragment.setmListIndex(0);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.legs_container, legsFragment)
+                    .commit();
+        }
     }
 }
